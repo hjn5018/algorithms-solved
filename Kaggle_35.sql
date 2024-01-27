44ce94e5c6297fd57038eff9990753298dfa018c
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-27 21:54:01 +0900

    Create Kaggle_35.sql

---
+++ Kaggle_35.sql
@@ -0,0 +1,6 @@
+-- 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 보호 기간이 긴 순으로 조회해야 합니다.
+
+SELECT i.animal_id, i.name
+from animal_ins i left join animal_outs o on i.animal_id = o.animal_id
+order by o.datetime-i.datetime desc
+limit 2
