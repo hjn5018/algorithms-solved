045bf00e5ea65925aa24fa9c5676184bb7343097
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-27 22:00:53 +0900

    Create Kaggle_36.sql

---
+++ Kaggle_36.sql
@@ -0,0 +1,6 @@
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다. 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+
+SELECT i.animal_id, i.animal_type, i.name
+from animal_ins i left join animal_outs o on i.animal_id = o.animal_id
+where i.sex_upon_intake like '%intact%' and (o.sex_upon_outcome like 'neutered%' or o.sex_upon_outcome like 'spayed%')
+order by i.animal_id
