24c4d2ea4a5a795650a296452883b3d85cd945c6
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-27 15:56:37 +0900

    Create Kaggle_26.sql

---
+++ Kaggle_26.sql
@@ -0,0 +1,7 @@
+-- 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 이때 결과는 시간대 순으로 정렬해야 합니다.
+
+SELECT substr(datetime, 12,2) HOUR, count(1) COUNT
+from animal_outs
+where substr(datetime, 12,2) between 9 and 19
+group by 1
+order by 1
