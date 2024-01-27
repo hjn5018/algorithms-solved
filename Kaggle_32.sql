45fa5f56c4575630627cc972e34c99c3f3b1730d
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-27 21:29:30 +0900

    Create Kaggle_32.sql

---
+++ Kaggle_32.sql
@@ -0,0 +1,7 @@
+-- 2022년 1월의 카테고리 별 도서 판매량을 합산하고, 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력하는 SQL문을 작성해주세요. 결과는 카테고리명을 기준으로 오름차순 정렬해주세요.
+
+SELECT B.CATEGORY, SUM(S.SALES)
+FROM BOOK B LEFT JOIN BOOK_SALES S ON B.BOOK_ID = S.BOOK_ID
+WHERE SUBSTR(S.SALES_DATE, 6, 2) = "01"
+GROUP BY B.CATEGORY
+ORDER BY B.CATEGORY
