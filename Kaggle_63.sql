43ec4c1da1c0e43ec19d2e038c6e24ba4526cc66
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-29 18:25:56 +0900

    Create Kaggle_63.sql

---
+++ Kaggle_63.sql
@@ -0,0 +1,12 @@
+-- CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 평균 대여 기간이 7일 이상인 자동차들의 자동차 ID와 평균 대여 기간(컬럼명: AVERAGE_DURATION) 리스트를 출력하는 SQL문을 작성해주세요. 평균 대여 기간은 소수점 두번째 자리에서 반올림하고, 결과는 평균 대여 기간을 기준으로 내림차순 정렬해주시고, 평균 대여 기간이 같으면 자동차 ID를 기준으로 내림차순 정렬해주세요.
+
+select *
+from
+(
+SELECT car_id, round(avg(datediff(end_date, start_date)+1), 1) average_duration
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by car_id
+order by car_id
+) a
+where average_duration >= 7
+order by 2 desc, 1 desc
