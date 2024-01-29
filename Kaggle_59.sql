3af5a5af2eb4438031e814968d0e961848f7534d
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-29 12:47:50 +0900

    Create Kaggle_59.sql

---
+++ Kaggle_59.sql
@@ -0,0 +1,16 @@
+-- CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 2022년 10월 16일에 대여 중인 자동차인 경우 '대여중' 이라고 표시하고, 대여 중이지 않은 자동차인 경우 '대여 가능'을 표시하는 컬럼(컬럼명: AVAILABILITY)을 추가하여 자동차 ID와 AVAILABILITY 리스트를 출력하는 SQL문을 작성해주세요. 이때 반납 날짜가 2022년 10월 16일인 경우에도 '대여중'으로 표시해주시고 결과는 자동차 ID를 기준으로 내림차순 정렬해주세요.
+
+select car_id, if(max = 1, "대여중", "대여 가능") AVAILABILITY
+from
+(
+select car_id, max(availability) max
+from
+(SELECT CAR_ID,
+       case when start_date <= '2022-10-16 00:00:00' and end_date >= '2022-10-16 00:00:00' then 1
+            when start_date > '2022-10-16 00:00:00' or end_date < '2022-10-16 00:00:00' then 0
+            end AVAILABILITY
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+) a
+group by 1
+) b
+order by 1 desc
