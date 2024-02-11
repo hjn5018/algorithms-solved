4987063900263fb7bb210f909ccc41d3f34736e9
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-02-11 09:59:58 +0900

    Create leetcode_626.sql

---
+++ leetcode_626.sql
@@ -0,0 +1,12 @@
+-- Write a solution to swap the seat id of every two consecutive students.
+-- If the number of students is odd, the id of the last student is not swapped.
+-- Return the result table ordered by id in ascending order.
+
+select
+case when id % 2 = 1 and id in (select max(id) from Seat) then id
+     when id % 2 = 1 then id + 1
+     else id - 1
+     end as id,
+     student
+from Seat
+order by id
