65e0b972eec8c50c785f0540cad211257466263b
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-06-30 20:45:07 +0900

    Create 가채점.py

---
+++ 가채점.py
@@ -0,0 +1,11 @@
+def solution(numbers, our_score, score_list):
+    answer = []
+    for i in range(len(numbers)):
+        if our_score[i] == score_list[numbers[i]-1]:
+            answer.append("Same")
+        else:
+            answer.append("Different")
+    
+    return answer
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/250128
