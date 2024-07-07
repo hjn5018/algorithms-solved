9417f44d4c901b223b7fe7cbe416b9a8e0d70954
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-07-07 12:20:51 +0900

    Update 추억_점수.py

--- 추억_점수.py
+++ 추억_점수.py
@@ -8,3 +8,16 @@ def solution(name, yearning, photo):
         answer.append(score)
     return answer
 # https://school.programmers.co.kr/learn/courses/30/lessons/176963
+
+def solution(name, yearning, photo):
+    answer = []
+    yearning_dict = dict(zip(name, yearning))
+    for i in photo:
+        score = 0
+        for j in i:
+            if j in name:
+                score += yearning_dict[j]
+        answer.append(score)
+    return answer
+
+# 계속 index를 쓰는 것보다 한번 zip을 해두고 써먹는 게 낫지 않을까?
