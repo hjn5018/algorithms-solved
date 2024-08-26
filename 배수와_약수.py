aa3e507ced5e088e6382ef686be2d5ad30f6cf8f
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-08-27 06:05:10 +0900

    Create 배수와_약수.py

---
+++ 배수와_약수.py
@@ -0,0 +1,12 @@
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    else:
+        if a % b == 0:
+            print("multiple")
+        elif b % a == 0:
+            print("factor")
+        else:
+            print("neither")
+# https://www.acmicpc.net/problem/5086
