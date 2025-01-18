a1503274256d8fb608ce0355e35dc3224b2af751
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2025-01-18 12:33:09 +0900

    Create 신나는_함수_실행.py

---
+++ 신나는_함수_실행.py
@@ -0,0 +1,24 @@
+def w(a:int, b:int, c:int):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    
+    if memo[a][b][c] != 0:
+        return memo[a][b][c]
+    
+    if a < b < c:
+        memo[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return memo[a][b][c]
+    else:
+        memo[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        return memo[a][b][c]
+
+memo = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+while True:
+    a, b, c = map(int, input().split())
+    if (a, b, c) == (-1, -1, -1):
+        break
+    print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
+
+# https://www.acmicpc.net/problem/9184
