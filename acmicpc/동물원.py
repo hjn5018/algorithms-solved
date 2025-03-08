059a44260885b4d4b9ffafd6e30f635aae607c8c
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2025-03-08 14:46:19 +0900

    Create 동물원.py

---
+++ acmicpc/동물원.py
@@ -0,0 +1,14 @@
+N = int(input())
+if N == 1:
+    print(3)
+    quit()
+
+dp = [0] * (N + 1)
+dp[1] = 3
+dp[2] = 7
+for n in range(3, N + 1):
+    dp[n] = (dp[n - 2] * 3 + (dp[n - 1] - dp[n - 2]) * 2)%9901
+
+print(dp[N])
+
+# https://www.acmicpc.net/problem/1309
